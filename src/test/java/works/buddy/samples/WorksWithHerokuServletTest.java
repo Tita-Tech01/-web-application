e27e304a15bc4411193e51caa968ca7b7e9d6858
Bob <99846946+bobafro@users.noreply.github.com>
2023-01-29 15:44:02 -0500

    Update WorksWithHerokuServletTest.java

--- src/test/java/works/buddy/samples/WorksWithHerokuServletTest.java
+++ src/test/java/works/buddy/samples/WorksWithHerokuServletTest.java
@@ -36,6 +36,6 @@ public class WorksWithHerokuServletTest {
         when(response.getWriter()).thenReturn(writer);
 
         servlet.doGet(request, response);
-        assertEquals("Buddy Works with Heroku", new String( out.toByteArray(), "UTF-8"));
+        assertEquals("If you are seeing this, it means i(Bob Afro) succeeded in deploying my web application to tomcat using jenkins", new String( out.toByteArray(), "UTF-8"));
     }
 }
